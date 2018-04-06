f1451da373528ba6c8e283e38f906ca19c575139
mtotodev05 <mtotodev05@gmail.com>
2018-04-07 01:49:00 +0300

    Added Classes

--- app/src/main/java/dev/brian/com/drawer_navigation/DrawHeader.java
+++ app/src/main/java/dev/brian/com/drawer_navigation/DrawHeader.java
@@ -1,7 +1,12 @@
 package dev.brian.com.drawer_navigation;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.mindorks.placeholderview.annotations.Layout;
 import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
 
 /**
  * Created by brian-kamau on 4/7/18.
@@ -9,4 +14,19 @@ import com.mindorks.placeholderview.annotations.NonReusable;
 @NonReusable
 @Layout(R.layout.drawer_header)
 public class DrawHeader {
+
+    @View(R.id.profileImageView)
+    private ImageView profileImage;
+
+    @View(R.id.nameTxt)
+    private TextView nameTxt;
+
+    @View(R.id.emailTxt)
+    private TextView emailTxt;
+
+    @Resolve
+    private void onResolved(){
+        nameTxt.setText("Brian Kamau");
+        emailTxt.setText("Briankamau151659@daystar.ac.ke");
+    }
 }
