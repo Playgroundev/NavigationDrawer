2a681036e09d2799e238fc3b5d9c0cd29831f115
mtotodev05 <mtotodev05@gmail.com>
2018-04-07 01:33:36 +0300

    Drawer Item

---
+++ app/src/main/java/dev/brian/com/drawer_navigation/DrawHeader.java
@@ -0,0 +1,12 @@
+package dev.brian.com.drawer_navigation;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+
+/**
+ * Created by brian-kamau on 4/7/18.
+ */
+@NonReusable
+@Layout(R.layout.drawer_header)
+public class DrawHeader {
+}
